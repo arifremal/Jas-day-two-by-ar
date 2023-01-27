8a62f00f5755d0def2f7666dc712d1e183b026c1
arifremal <arifremal013@gmail.com>
2023-01-27 15:23:03 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,21 @@
+// number type variable
+var name = 23;
+
+// string type variable
+var specialName = 'Arif ';
+//  Boolean variable
+var isCorrect = true;
+// two types of number : integer => 120
+// float ,decimal : 3.65,
+// variable naming convention: rules,beastpractice
+//  1. single word
+// /2.no gap or space
+// no quote
+// no keuword
+// can not contain dash
+// should use  camel
+// math operator: +,-,*,/
+// shorthand : +=,-=,++,--
+// convert : parseInt,parseFloat
+// toFixed
+// reminder : vagsesh : %
